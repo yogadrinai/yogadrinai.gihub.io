1181aa6310aa060e9270bcbe48fbddb37c402d71
Adrinai-digital <adrinai.conchi@gmail.com>
2024-04-23 00:28:08 +0200

    actualización post

--- js/news_post_custom.js
+++ js/news_post_custom.js
@@ -180,3 +180,18 @@ $(document).ready(function()
 		}
 	}
 });
+document.addEventListener('DOMContentLoaded', function () {
+    const accordionItems = document.querySelectorAll('.accordion-item');
+
+    accordionItems.forEach(item => {
+        item.addEventListener('click', function () {
+            item.classList.toggle('active');
+            const content = item.querySelector('.accordion-content');
+            if (item.classList.contains('active')) {
+                content.style.display = 'block';
+            } else {
+                content.style.display = 'none';
+            }
+        });
+    });
+});
